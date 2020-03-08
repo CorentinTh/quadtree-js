aff950d43b5c2a3fad11a04a5fff9f7785592f7e
CorentinTh <corentin.thomasset74@gmail.com>
2020-03-08 11:56:49 +0100

    refactor: Circle in typescript

--- src/Circle.ts
+++ src/Circle.ts
@@ -1,9 +1,13 @@
-
 /**
  * Box Circle.
  * @class Circle
  */
-class Circle {
+export class Circle {
+    readonly x: number;
+    readonly y: number;
+    readonly r: number;
+    readonly rPow2: number;
+    readonly data: unknown;
 
     /**
      * Circle constructor;
@@ -13,15 +17,15 @@ class Circle {
      * @param {number} r - Radius of the circle.
      * @param {*} [data] - Data to store along the circle.
      */
-    constructor(x, y, r, data) {
+    constructor(x: number, y: number, r: number, data?: unknown) {
         this.x = x;
         this.y = y;
         this.r = r;
         this.rPow2 = this.r * this.r; // To avoid square roots
-        if (data) this.data = data;
+        this.data = data;
     }
 
-    _euclideanDistancePow2(point1, point2) {
+    _euclideanDistancePow2(point1, point2): number {
         return Math.pow((point1.x - point2.x), 2) + Math.pow((point1.y - point2.y), 2);
     }
 
@@ -30,7 +34,7 @@ class Circle {
      * @param {Point|Object} point - The point to test if it is contained in the circle.
      * @returns {boolean} - True if the point is contained in the circle, otherwise false.
      */
-    contains(point) {
+    contains(point): boolean {
         return this._euclideanDistancePow2(point, this) <= this.rPow2;
     }
 
@@ -39,14 +43,12 @@ class Circle {
      * @param {Box|Object} range - The box to test the intersection with.
      * @returns {boolean} - True if it intersects, otherwise false.
      */
-    intersects(range) {
-        const Max = (a, b) => a >= b ? a : b;
-        const Min = (a, b) => a <= b ? a : b;
+    intersects(range): boolean {
+        const Max = (a: number, b: number): number => a >= b ? a : b;
+        const Min = (a: number, b: number): number => a <= b ? a : b;
 
         const dX = this.x - Max(range.x, Min(this.x, range.x + range.w));
         const dY = this.y - Max(range.y, Min(this.y, range.y + range.h));
         return (dX * dX + dY * dY) <= (this.rPow2);
     }
 }
-
-module.exports = Circle;
