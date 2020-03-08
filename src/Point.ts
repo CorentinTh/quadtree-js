ceee36dd6e0364a6e16c246077295a0da8e7368f
CorentinTh <corentin.thomasset74@gmail.com>
2020-03-08 11:52:37 +0100

    refactor: Point in typescript

--- src/Point.ts
+++ src/Point.ts
@@ -1,9 +1,11 @@
-
 /**
  * Point class.
  * @class Point
  */
-class Point {
+export class Point {
+    x: number;
+    y: number;
+    data: unknown;
 
     /**
      * Point constructor.
@@ -12,12 +14,10 @@ class Point {
      * @param {number} y - Y coordinate of the point.
      * @param {*} [data] - Data to store along the point.
      */
-    constructor(x, y, data) {
+    constructor(x: number, y: number, data?: unknown) {
         this.x = x;
         this.y = y;
-        if (data) this.data = data;
+        this.data = data;
     }
 
 }
-
-module.exports = Point;
